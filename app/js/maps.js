82da2c07fb3a37af862599c0315263eb7914a58a
sisay82 <sisay82@gmail.com>
2018-01-18 00:27:17 +0100

    Fixed loading of google widget and his resize

--- app/js/maps.js
+++ app/js/maps.js
@@ -1,74 +1,72 @@
-(function () {
-    'use strict';
+'use strict';
+/* jshint browser:true */
+var myHome = { lat: 44.357267, lng: 11.717141 };
+var myCenter = { lat: 44.357267, lng: 11.717141 };
 
-    var myHome = { lat: 44.357267, lng: 11.717141 };
-    var myCenter = { lat: 44.357267, lng: 11.717141 };
+var coords = [
+    { lat: 44.449345, lng: 11.818122 },
+    { lat: 44.319702, lng: 11.799388 },
+    { lat: 44.212769, lng: 11.504638 },
+    { lat: 44.402830, lng: 11.249503 },
+    { lat: 44.492093, lng: 11.211421 },
+    { lat: 44.545555, lng: 11.194679 },
+    { lat: 44.580958, lng: 11.366102 },
+    { lat: 44.588357, lng: 11.566579 }
+];
 
-    var coords = [
-        { lat: 44.449345, lng: 11.818122 },
-        { lat: 44.319702, lng: 11.799388 },
-        { lat: 44.212769, lng: 11.504638 },
-        { lat: 44.402830, lng: 11.249503 },
-        { lat: 44.492093, lng: 11.211421 },
-        { lat: 44.545555, lng: 11.194679 },
-        { lat: 44.580958, lng: 11.366102 },
-        { lat: 44.588357, lng: 11.566579 }
-    ];
+var map;
+var google = google || {};
+var fullWidth = fullWidth || {};
+function resizeMap() {
+    if (map) {
+        myCenter = map.getCenter();
 
-    var map;
+        // center map adaptive to resolution
+        fullWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    function resizeMap() {
-        if (map) {
-            myCenter = map.getCenter();
-
-            // center map adaptive to resolution
-            fullWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-            if (fullWidth >= 500) {
-                myCenter = new google.maps.LatLng(44.395415, 11.504676); //>500px
-            }
-            else {
-                myCenter = new google.maps.LatLng(44.395415, 11.504676 + (5 - fullWidth / 100) / 100); //min value
-            }
-
-            map.setZoom(10);
-            map.panTo(myCenter);
-            google.maps.event.trigger(map, 'resize');
-            map.setCenter(myCenter);
+        if (fullWidth >= 500) {
+            myCenter = new google.maps.LatLng(44.395415, 11.504676); //>500px
         }
+        else {
+            myCenter = new google.maps.LatLng(44.395415, 11.504676 + (5 - fullWidth / 100) / 100); //min value
+        }
+
+        map.setZoom(10);
+        map.panTo(myCenter);
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(myCenter);
     }
+}
 
-    function initMap() {
-        var mapProp = {
-            center: myCenter,
-            zoom: 10,
-            scrollwheel: false,
-            draggable: true,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+function initMap() {
+    var mapProp = {
+        center: myCenter,
+        zoom: 10,
+        scrollwheel: false,
+        draggable: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-        map = new google.maps.Map(document.getElementById("mapCanvas"), mapProp);
+    map = new google.maps.Map(document.getElementById("mapCanvas"), mapProp);
 
-        var fisioDomusWorkingArea = new google.maps.Polygon({
-            paths: coords,
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 3,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35
-        });
+    var fisioDomusWorkingArea = new google.maps.Polygon({
+        paths: coords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
 
-        fisioDomusWorkingArea.setMap(map);
+    fisioDomusWorkingArea.setMap(map);
 
-        var marker = new google.maps.Marker({
-            position: myHome,
-            map: map,
-            title: 'FisioDomus'
-        });
+    var marker = new google.maps.Marker({
+        position: myHome,
+        map: map,
+        title: 'FisioDomus'
+    });
 
-        resizeMap();
+    resizeMap();
 
-        google.maps.event.addDomListener(window, 'resize', resizeMap);
-    }
-
-})();
+    google.maps.event.addDomListener(window, 'resize', resizeMap);
+}
--- app/js/resizing.js
+++ app/js/resizing.js
@@ -1,64 +1,64 @@
-(function () {
-    'use strict';
 
-    var fullWidth = 1024;
-    var fullHeight = 768;
-    var adaptiveFullScreenThreshold = 768;
+'use strict';
+/* jshint browser:true */
 
-    switch (document.readyState) {
-        case "interactive":
-        case "complete":
-            if (typeof fullScreen === "function") fullScreen(event);
-            if (typeof heightsEqualizer === "function") heightsEqualizer(event);
-            break;
-        default:
-            document.addEventListener("readystatechange", function (event) {
-                if (typeof fullScreen === "function") fullScreen(event);
-                if (typeof heightsEqualizer === "function") heightsEqualizer(event);
-            });
-    }
+var fullWidth = 1024;
+var fullHeight = 768;
+var adaptiveFullScreenThreshold = 768;
 
-
-    window.addEventListener("resize", function (event) {
+switch (document.readyState) {
+    case "interactive":
+    case "complete":
         if (typeof fullScreen === "function") fullScreen(event);
         if (typeof heightsEqualizer === "function") heightsEqualizer(event);
-    });
+        break;
+    default:
+        document.addEventListener("readystatechange", function (event) {
+            if (typeof fullScreen === "function") fullScreen(event);
+            if (typeof heightsEqualizer === "function") heightsEqualizer(event);
+        });
+}
 
-    function heightsEqualizer(event) {
-        var elements = document.querySelectorAll("[data-equalizeHeight=true]"),
-            max_height = 0,
-            len = 0,
-            i;
 
-        if (elements && elements.length > 0) {
-            len = elements.length;
+window.addEventListener("resize", function (event) {
+    if (typeof fullScreen === "function") fullScreen(event);
+    if (typeof heightsEqualizer === "function") heightsEqualizer(event);
+});
 
-            for (i = 0; i < len; i++) { // get max height
-                elements[i].style.height = ''; // reset height attr
-                if (elements[i].clientHeight > max_height) {
-                    max_height = elements[i].clientHeight;
-                }
-            }
+function heightsEqualizer(event) {
+    var elements = document.querySelectorAll("[data-equalizeHeight=true]"),
+        max_height = 0,
+        len = 0,
+        i;
 
-            for (i = 0; i < len; i++) { // set max height to all elements
-                elements[i].style.height = max_height + 'px';
-            }
-        }
-    }
+    if (elements && elements.length > 0) {
+        len = elements.length;
 
-    function fullScreen(event) {
-        fullWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-        fullHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-        // Determine whether width or height should be 100%
-        var domElements = document.querySelectorAll("[data-fullscreen=true]");
-        if (domElements && domElements.length > 0) {
-            for (i = 0; i < domElements.length; i++) {
-                var showNext = 0;
-                if (fullHeight > adaptiveFullScreenThreshold) showNext = 120;
-                domElements[i].style.height = fullHeight - showNext + "px";
+        for (i = 0; i < len; i++) { // get max height
+            elements[i].style.height = ''; // reset height attr
+            if (elements[i].clientHeight > max_height) {
+                max_height = elements[i].clientHeight;
             }
         }
+
+        for (i = 0; i < len; i++) { // set max height to all elements
+            elements[i].style.height = max_height + 'px';
+        }
     }
-})();
+}
+
+function fullScreen(event) {
+    fullWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    fullHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    // Determine whether width or height should be 100%
+    var domElements = document.querySelectorAll("[data-fullscreen=true]");
+    if (domElements && domElements.length > 0) {
+        for (var i = 0; i < domElements.length; i++) {
+            var showNext = 0;
+            if (fullHeight > adaptiveFullScreenThreshold) showNext = 120;
+            domElements[i].style.height = fullHeight - showNext + "px";
+        }
+    }
+}
