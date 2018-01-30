5e2d627079f06ff26303b3ec491c6abf061acb60
sisay82 <sisay82@gmail.com>
2018-01-30 23:19:24 +0100

    Fixed contact form for automatic mails

--- app/js/components/contacts/contacts.js
+++ app/js/components/contacts/contacts.js
@@ -8,21 +8,43 @@
         .directive("fdContacts", contactsDirective)
         .controller('contactsCtrl', contactsCtrlFn);
 
-    function contactsDirective(){
+    function contactsDirective() {
         return {
-            scope:{
+            scope: {
             },
             restrict: 'E',
-             templateUrl: '/js/components/contacts/contacts.temp.html',
-            controller:'contactsCtrl',
+            templateUrl: '/js/components/contacts/contacts.temp.html',
+            controller: 'contactsCtrl',
             controllerAs: 'ctrl'
-        };    
+        };
     }
 
-    function contactsCtrlFn($scope, $rootScope) {
+    function contactsCtrlFn($scope, $rootScope, $http) {
+        var contactServURL = "/php-services/contact-form.php";
         $scope.user = {};
 
-        this.openPrivacy = function() {
+        this.submitContactReq = function (isValid) {
+            if (isValid) {
+                var dataToSend = {
+                    "name": $scope.user.firstname + " " + $scope.user.surname,
+                    "phoneNumber": $scope.user.phoneNumber,
+                    "email": $scope.user.email,
+                    "message": $scope.user.message,
+                    "privacyAgree": $scope.user.privacyAgree
+                };
+
+                $http.post(contactServURL, dataToSend).then(function (resolve) {
+                        $scope.status = resolve.status;
+                        $scope.data = resolve.data;
+                        $scope.result = resolve.data; // Show result from server in our <pre></pre> element and clean the form
+                    });
+            } else {
+                alert('Form is not valid');
+            }
+
+        };
+
+        this.openPrivacy = function () {
             $rootScope.$broadcast('openPrivacyRequest');
         };
     }
--- app/php-services/contact-form.php
+++ app/php-services/contact-form.php
@@ -1,11 +1,39 @@
 <?php
 require_once '../wordpress/wp-includes/class-phpmailer.php';
+//Make sure that it is a POST request.
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
+    $data = array('success' => false, 'message' => 'Request method must be POST!');
+    echo json_encode($data);
+    exit;
+}
 
-if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+//Make sure that the content type of the POST request has been set to application/json
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if(stripos($contentType, 'application/json') === false){
+    $data = array('success' => false, 'message' => 'Content type must be: application/json');
+    echo json_encode($data);
+    exit;
+}
 
+//Receive the RAW post data.
+$content = trim(file_get_contents("php://input"));
+
+//Attempt to decode the incoming RAW post data from JSON.
+$decoded = json_decode($content, true);
+
+//If json_decode failed, the JSON is invalid.
+if(!is_array($decoded)){
+    $data = array('success' => false, 'message' => 'Received content contained invalid JSON!');
+    echo json_encode($data);
+    exit;
+}
+
+$_POST=$decoded;
+
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) && isset($_POST['privacyAgree'])) {
     //check if any of the inputs are empty
     if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phoneNumber']) || empty($_POST['message'])) {
-        $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+        $data = array('success' => false, 'message' => 'Please fill out the form completely. Some inputs are empty.');
         echo json_encode($data);
         exit;
     }
@@ -17,7 +45,15 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']))
     $mail->SetFrom($_POST['email'], $_POST['name']);
     $mail->AddAddress('info@fisiodomus.it'); //recipient 
     $mail->Subject = 'Richiesta di intervento di ' .  $_POST['name'] . ' del '. date("d/m/Y"); //Subject 
-    $mail->Body = "Nome: " . $_POST['name'] . "\r\n E-mail: " . $_POST['email'] . "\r\n Numero di telefono: " . $_POST['phoneNumber'] . "\r\n Comune: " . $_POST['municipality'] . "\r\n\r\n Message: " . stripslashes($_POST['message']);
+    $mail->Body = "Nome: " . $_POST['name'] . "\r\n E-mail: " . $_POST['email'] . "\r\n Numero di telefono: " . $_POST['phoneNumber'] . "\r\n Comune: " . $_POST['municipality'] ;
+    
+    if($_POST['privacyAgree']) {
+        $mail->Body = $mail->Body. "\r\n Informativa sulla privacy: accettata.";
+    } else {
+        $mail->Body = $mail->Body. "\r\n Informativa sulla privacy: rifiutata.";
+    }
+
+    $mail->Body = $mail->Body. "\r\n\r\n Message: " . stripslashes($_POST['message']);
 
 
     if(!$mail->send()) {
@@ -30,7 +66,6 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']))
     echo json_encode($data);
 
 } else {
-
     $data = array('success' => false, 'message' => 'Please fill out the form completely.');
     echo json_encode($data);
 
